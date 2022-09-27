b14583f612069be82cec590eaee06b31076b315b
tareita <tareitaa@outlook.com>
2022-09-27 21:23:09 +0100

    frequency

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,15 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
+from collections import Counter
 
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        if str(item) not in frequencies:
+            frequencies[str(item)] = 0
+        frequencies[str(item)] += 1   
+
     return frequencies
+
+# print(frequencies(['0', 4,4,'4','d','d','e',0,'a','d','4']))
